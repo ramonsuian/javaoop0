0f09c8b67f93e082ff24627287b2d40662318e17
ramonsuian <35726788@qq.com>
2018-05-03 17:33:07 +0800

    偶数100

--- 作业/src/作业/Java04.java
+++ 作业/src/作业/Java04.java
@@ -1,5 +1,20 @@
 package 作业;
 
 public class Java04 {
+	public static void main(String[] args) {
+		int i = 1;
+		while (i <= 100) {
+			int k = i % 2;
+			int p = i % 10;
+			if (k == 0) {
+				System.out.print(i + "\t"); // t字表符
 
+			}
+			if (p == 0) {
+				System.out.println("");
+			}
+			i++;//增量 不输入就无限输出。
+		}
+
+	}
 }
